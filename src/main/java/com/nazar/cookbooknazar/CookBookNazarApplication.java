164a35197c4ddd7183fcfab76807001461d7fe39
Altanim <altanim0404@gmail.com>
2023-01-10 20:54:11 +0300

    initial commit

---
+++ src/main/java/com/nazar/cookbooknazar/CookBookNazarApplication.java
@@ -0,0 +1,13 @@
+package com.nazar.cookbooknazar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CookBookNazarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CookBookNazarApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nazar/cookbooknazar/CookBookNazarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nazar.cookbooknazar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CookBookNazarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
